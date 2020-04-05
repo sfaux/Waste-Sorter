518907615a9b1c3939d93eb7e46af81f5da6dec2
Sydney Faux <48231621+sfaux@users.noreply.github.com>
2020-04-04 20:33:11 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1tbgu_grGPQP9kUzEhkf-lX49X4E-AkEB'
-export_file_name = 'export.pkl'
+export_file_name = 'waste-sorter.pkl'
 
 classes = ['cardboard','glass','metal','paper','plastic','trash']
 path = Path(__file__).parent
