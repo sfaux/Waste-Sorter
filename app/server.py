71cbc61068f8950f985d21c2b642db71bd3d57ff
Sydney Faux <48231621+sfaux@users.noreply.github.com>
2020-04-04 20:20:40 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=163lU2vEmzS0N54pfWp2lYfypUXzGHfL5'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1tbgu_grGPQP9kUzEhkf-lX49X4E-AkEB'
 export_file_name = 'export.pkl'
 
 classes = ['cardboard','glass','metal','paper','plastic','trash']
